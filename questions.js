008efc8bb5f5c4b9b549f9fc3a38ab80dc08e5cd
sakthipriya1 <sakthipriya826@gmail.com>
2021-09-25 11:15:44 +0530

    intialcommit

---
+++ questions.js
@@ -0,0 +1,42 @@
+var questions=[
+    {
+        "question":"Grand Central Terminal ,Park Avenue,Newyork is the world's",
+        "option1":"largest railway station",
+        "option2":"hihest  railway station",
+        "option3":"longest  railway station",
+        "option4":"None of the above",
+        "answer":"1"
+    },
+    {
+        "question":"Eritrea,which became the 182nd memeber of the UN in 1993,is in the continent of",
+        "option1":"Asia",
+        "option2":"Africa",
+        "option3":"Europe",
+        "option4":"Australia",
+        "answer":"2"
+    },
+    {
+        "question":"First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky,was conducted in",
+        "option1":"1967",
+        "option2":"1968",
+        "option3":"1958",
+        "option4":"1922",
+        "answer":"4"
+    },
+    {
+        "question":"Federation Cup,World Cup,Allywyn Internation Trophy and Challenge Cup are awarded to winners of",
+        "option1":"Tennis",
+        "option2":"Basketball",
+        "option3":"Volleyball",
+        "option4":"Cricket",
+        "answer":"3"
+    },
+    {
+        "question":"Each year World REd Cross and REd Crescent Day is Celebrated on",
+        "option1":"May 8",
+        "option2":"May 18",
+        "option3":"June 18",
+        "option4":"Junw 8",
+        "answer":"1"
+    },
+]
